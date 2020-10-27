8c6aac7d99d92ecf63df74d829327030c71f56c6
Aaron <ng.h@husky.neu.edu>
2020-10-27 17:22:47 -0400

    Adding hw06 code?:

---
+++ hw06/hw06/brain/viz.hh
@@ -0,0 +1,8 @@
+#ifndef VIZ_H
+#define VIZ_H
+
+int viz_run(int argc, char **argv);
+int viz_hit(float range, float angle);
+int viz_drawXY(float x, float y);
+
+#endif
---
+++ hw06/ngh_hw06/README-HW06.md
@@ -0,0 +1,2 @@
+Ho Yat Aaron, Ng
+Youtube URL: https://www.youtube.com/watch?v=uZ_fFPHE3MM&feature=youtu.be
